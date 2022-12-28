253335b0b111322db1c9e751896af5d0691a2989
Pushpak Kumawat <95577067+1pushpak1@users.noreply.github.com>
2022-12-28 16:34:50 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Portfolio | Pushpak";
-            $("#favicon").attr("href", "assets/images/favicon1.png");
+            $("#favicon").attr("href", "/assets/images/favicon1.png");
         }
         else {
             document.title = "Come Back To Portfolio";
